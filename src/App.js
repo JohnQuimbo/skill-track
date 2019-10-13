709ad9c2aea063de2b2b8ed5fa1f0e023d32a79a
John Quimbo <johnanthony.quimbo@gmail.com>
2019-10-13 00:13:41 -0400

    Finally got the component to work

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Skill from "./Skill"
-import drumChallenges from "./drumChallenges"
+
+const drumChallenges = [
+  {
+      id:1,
+      text:"Set up your first drum set"
+  },
+  {
+      id:2,
+      text:"Learn how to properly grip sticks"
+  },
+  {
+      id:3,
+      text:"Hit drums with proper technique"
+  },
+  {
+      id:4,
+      text:"4/4 beat (1 +3 on Bass Drum/ 2 + 4 on snare)"
+      //Song challenge(Wake Me Up When September Ends)
+  },
+  {
+      id:5,
+      text:"Learn Basic Rock beat"
+      //Song challenge(Back in Black)
+  }
+
+]
 
 class App extends React.Component {
   constructor(){
     super()    
     this.state = {
-      count : 1,
-      challenges: drumChallenges
+      count : 1   
     }
     this.handleClick = this.handleClick.bind(this)
   }
@@ -20,19 +44,16 @@ class App extends React.Component {
         count : prevState.count + 1
       }
     })
-
-
-
   };
-
-
+  
   render(){
+    const challenge = drumChallenges[this.state.count];
     return(
       <div className="App">
         <Skill 
           name="Drums" 
           imgUrl="https://images-na.ssl-images-amazon.com/images/I/81kXfzLBExL._SX425_.jpg" 
-          nextChallenge="Proper stick control"
+          nextChallenge={challenge.text}
           level= {this.state.count}
           onClick={this.handleClick}  
         />
